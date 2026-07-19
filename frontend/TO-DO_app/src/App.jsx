da2b1ca683e02190dfad2157becff554370c7e60
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-07-19 17:35:52 +0530

    update app.jsx and index.css

--- frontend/TO-DO_app/src/App.jsx
+++ frontend/TO-DO_app/src/App.jsx
@@ -1,5 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Register_user from "./register/register.jsx";
+import Login from "./login/login.jsx";
 function App(){
   return <>
+ <Routes>
+
+      <Route path="/" element={<Register_user />} />
+
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
   </>
 }
 export default App;
